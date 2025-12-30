cf9bda09d70e978a680ed3cc80c0cd7b7ed85f4c
Luan Sousa <149454020+Luanhotlinebr@users.noreply.github.com>
2025-12-30 18:23:23 -0300

    solving challenges in challenge-10
    Refactor challenge-10.js to use proper types and IIFE.

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 Crie uma IIFE que envolva todo esse arquivo (inclusive esse comentário),
 e faça a indentação correta.
@@ -10,9 +11,11 @@ das variáveis. Analise o que está sendo impresso no console para saber como
 resolver o problema corretamente.
 */
 var five = '5';
+var five = Number('5');
 console.log( five + ' é número?', typeof five === 'number' );
 
 var concat = 10 + 10;
+var concat = String(10 + 10);
 console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
 /*
@@ -24,7 +27,23 @@ funcional, mas dessa vez, separando algumas responsabilidades.
 função receberá dois parâmetros e retornará a operação referente à sua
 propriedade, usando os valores passados por parâmetro.
 */
-// ?
+var operation = {
+    "+":function(number1,number2){
+        return number1 + number2;
+    },
+    "-": function(number1,number2){
+        return number1 - number2;
+    },
+    "*":function(number1,number2){
+        return number1 * number2;
+    },
+    "/":function(number1,number2){
+        return number1 / number2;
+    },
+    "%":function(number1,number2){
+        return number1 % number2;
+    }
+}
 
 /*
 Crie uma função chamada `isOperatorValid`, que receberá um operador por
@@ -36,8 +55,10 @@ parâmetro a ela é válido, ou seja, se ele é igual a '+', '-', '*', '/' ou
 Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
-// ?
-
+function isOperatorValid(operator){
+ return operation[operator] ? true : false;
+}
+  
 /*
 Agora vamos criar a calculadora.
 - Crie uma função chamada `calculator`, que receberá como parâmetro um
@@ -111,3 +132,5 @@ Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
 a mensagem de erro será mostrada no console.
 */
 // ?
+  
+}())
