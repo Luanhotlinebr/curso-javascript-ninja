c9c7277b1c6b7b3502c8bf26d191b7a0ccc9418e
Luanhotlinebr <luanhotlinebr60@gmail.com>
2026-02-04 07:50:05 -0300

    almost finished part 3 of refactoring

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -45,10 +45,16 @@ mesma funcionalidade.
   }
 
   function isLastItemAnOperation(number) {
-    var operations = ["+", "-", "x", "÷"];
+    var operations = getOperations();
     var lastItem = number.split("").pop();
     return operations.some(function (operator) {
       return operator === lastItem;
+    });
+  }
+
+  function getOperations() {
+    return Array.prototype.map.call($buttonsOperations, function (button) {
+      return button.value;
     });
   }
 
@@ -65,8 +71,12 @@ mesma funcionalidade.
   // Acoplamento - Funcao que depende de outra funcao para existir
   function handleClickEqual() {
     $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-    var allValues = $visor.value.match(/\d+[+x÷-]?/g);
+    var allValues = $visor.value.match(getRegexOperations());
     $visor.value = allValues.reduce(calculateAllValues);
+  }
+
+  function getRegexOperations() {
+    return new RegExp("\\d+[" + getOperations().join("") + "]?", "g");
   }
 
   function calculateAllValues(accumulated, actual) {
